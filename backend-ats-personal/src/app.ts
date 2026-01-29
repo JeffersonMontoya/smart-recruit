fd116e80656845a4bd1147a2bace585df6c3c551
JeffersonMontoya <112036905+JeffersonMontoya@users.noreply.github.com>
2026-01-29 14:49:35 -0700

    feat: initial monorepo setup  with react-ts and node-ts

---
+++ backend-ats-personal/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'API ATS funcionando 🚀' })
+})
+
+export default app
